8dc9b05bd0a8f91773f4f446437a7d256fc7de5b
Zedikiah <xyzed3k1ah@gmail.com>
2023-11-01 09:19:32 -0600

    arithmetic lab

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
+var num1 = 31;
+var num2 = 2;
+var multiply = num1 * num2;
+
+console.log(multiply);
+
+var random = Math.floor(Math.random() * Number.MAX_SAFE_INTEGER) + 1;
+
+console.log(random);
+
+var num3 = 14;
+var num4 = 5;
+var mod = num3 % num4;
+
+console.log(mod);
+
+var numbers = [10, 5, 20, 15, 7];
+var max = Math.max(...numbers);
+
+console.log(max);
 
 // Write your code here
